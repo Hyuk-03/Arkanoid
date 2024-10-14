8e86a47a92cb921322a85444abc350626521988b
Hyuk-03 <tkdgurqkr52@gmail.com>
2024-10-15 02:23:59 +0900

    Step19
    보스의 애니메이션 구현

--- Arkanoid/Assets/Scripts/BossCtrl.cs
+++ Arkanoid/Assets/Scripts/BossCtrl.cs
@@ -2,10 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum BossState
+{
+    Idle,
+    Attack,
+    Die
+}
 public class BossCtrl : MonoBehaviour
 {
     public int Hp;      //보스의 hp
     int ScoreValue;     //스코어
+
+    float AttackTimer;   //보스의 공격타이머
+    float AttackInterval = 2.0f;  //보스의 공격간격
+
+    Animator BossAnim;  //보스의 애니메이션
+    BossState m_BossState = BossState.Idle;  // 보스의 상태
 
     // Start is called before the first frame update
     void Start()
@@ -15,12 +27,21 @@ public class BossCtrl : MonoBehaviour
             Hp = 10;            //보스의 Hp는 10으로 설정
             ScoreValue = 100;   //보스의 점수
         }
+
+        BossAnim = GetComponent<Animator>();
+        AttackTimer = AttackInterval; // 초기 타이머 설정
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        AttackTimer -= Time.deltaTime;
+
+        if (m_BossState == BossState.Idle && AttackTimer <= 0)
+        {
+            Attack();
+        }
     }
 
     public void TakeDmg(int a_Value)
@@ -38,6 +59,27 @@ public class BossCtrl : MonoBehaviour
             GameMgr.Inst.AddScore(ScoreValue);  //점수획득
             Destroy(gameObject);
         }
+    }
+    void Attack()
+    {
+        m_BossState = BossState.Attack; // 상태를 Attack으로 변경
+        BossAnim.SetBool("attack", true); // 어택 애니메이션 시작
+        BossAnim.SetBool("nomal", false);  //노말상태가 아니다
+        StartCoroutine(AttackCoroutine());
+    
+    }
+    IEnumerator AttackCoroutine()
+    {
+        yield return new WaitForSeconds(1.0f); // 애니메이션의 길이에 맞게 조정
 
+        EndAttack(); // 공격이 끝난 후 Idle 상태로 돌아감
+        AttackTimer = AttackInterval; // 타이머 리셋
+    }
+
+    public void EndAttack()
+    {
+        BossAnim.SetBool("nomal", true);   //노말상태다
+        BossAnim.SetBool("attack", false); // 어택 애니메이션 종료
+        m_BossState = BossState.Idle; // 상태를 Idle로 변경
     }
 }
